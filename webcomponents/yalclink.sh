f9dfae68ecb7c5bb38d3e83a85fd6f4e49a1abb3
Mahdi <mahdi.ridho@gmail.com>
2022-08-21 21:16:20 +0700

    Yalc jc-chat

--- webcomponents/yalclink.sh
+++ webcomponents/yalclink.sh
@@ -5,10 +5,10 @@ set -e
 # make sure we have fresh link
 npm cache clean --force
 yalcDir=`yalc dir`
-rm -rf $yalcDir/packages/@jcshop/jc-checkout
+rm -rf $yalcDir/packages/@jcshop/jc-checkout $yalcDir/packages/@jcshop/jc-chat
 
 find . \( -name 'node_modules' \) -or \( -name 'package-lock.json' \) -or \( -name 'yalc.lock' \) -or \( -name '.yalc' \) | xargs rm -rf
-requirements="jc-checkout"
+requirements="jc-checkout jc-chat"
 for r in $requirements; do
   pushd $r;
   ./install.sh;
